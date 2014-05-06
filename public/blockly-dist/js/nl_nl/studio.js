db55f27501ec1362b031154b197aab57cbdd649a
deploy-code-org <dev@code.org>
2014-05-06 20:37:44 +0000

    Automatically built.

--- public/blockly-dist/js/nl_nl/studio.js
+++ public/blockly-dist/js/nl_nl/studio.js
@@ -1162,6 +1162,9 @@ exports.displayFeedback = function(options) {
   if (showCode) {
     feedback.appendChild(showCode);
   }
+  if (options.level.is_k1) {
+    feedback.className += " k1";
+  }
 
   feedback.appendChild(getFeedbackButtons(
     options.feedbackType, options.level.showPreviousLevelButton));
