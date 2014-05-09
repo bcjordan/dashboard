bd66a222d9dfd7fe26351214e215764995d0640f
Brendan Reville <brendan@code.org>
2014-05-08 23:02:15 -0700

    Better rails logging for string debugging.

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -82,7 +82,7 @@ module LocaleHelper
   # Looks up a localized string driven by a database value.
   # See config/locales/data.en.yml for details.
   def data_t(dotted_path, key)
-    puts "I18n.t(\"data.#{dotted_path}\")[\"#{key}\".to_sym]"
+    Rails.logger.info "I18n.t(\"data.#{dotted_path}\")[\"#{key}\".to_sym]"
     I18n.t("data.#{dotted_path}")[key.to_sym]
   end
 
