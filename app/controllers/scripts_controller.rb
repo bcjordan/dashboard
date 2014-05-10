bc32f2ed9df37b69b5edb7fd6fca00a4942d1f49
Will Jordan <will@code.org>
2014-05-09 17:31:49 -0700

    Add a form page to edit script text. Add a link to process all scripts.

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,16 +1,66 @@
 class ScriptsController < ApplicationController
   before_filter :authenticate_user!, except: :show
   check_authorization
+  load_and_authorize_resource
+  before_action :set_script, only: [:show, :edit, :update, :destroy]
+  before_action :set_script_file, only: [:edit, :update, :destroy]
 
   def index
-    authorize! :manage, Script
+    rake if params[:rake] == '1'
     # Show all the scripts that a user has created.
     @scripts = Script.all
   end
 
   def show
+  end
+
+  def edit
+    if @script.id <= Script::MAX_DEFAULT_LEVEL_ID
+      render :status => :forbidden, :text => "Default scripts not editable."
+    end
+  end
+
+  def update
+    respond_to do |format|
+      script_text = params[:script_text]
+      if @script.update_text(script_params, script_text)
+        format.html { redirect_to @script, notice: I18n.t('crud.updated', model: Script.model_name.human) }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @script.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def set_script_file
+    filename = "config/scripts/#{@script.name}.script"
+    @script_file = File.exists?(filename) && File.read(filename)
+  end
+
+  private
+
+  def rake
+    @errors = []
+    begin
+      Script.rake
+      redirect_to scripts_path, notice: "Updated."
+    rescue Exception => e
+      @errors << e.to_s
+      render action: 'index'
+    end
+  end
+
+  def set_script
     @script = Script.get_from_cache(params[:id])
-    
-    authorize! :read, @script
+  end
+
+  def script_params
+    params.require(:script).permit(:name)
+  end
+
+  prepend_before_filter do
+    params[:script] &&= script_params
   end
 end
