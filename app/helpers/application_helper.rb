1b44f2b71e3d4c36c0bbf251bf9f65ea6af54387
Brian Jordan <bcjordan@gmail.com>
2014-05-13 15:46:59 -0700

    Honor youtube base URL override

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,7 +76,7 @@ module ApplicationHelper
   end
   
   def youtube_base_url
-    'https://www.youtubeeducation.com'
+    @youtube_base_url || 'https://www.youtubeeducation.com'
   end
 
   def video_thumbnail_url(video)
